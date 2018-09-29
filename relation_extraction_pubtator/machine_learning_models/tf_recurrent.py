ecec07d118d4a0aa7faf92842a9d8e89c42f4167
kiblawi <kiblawi@wisc.edu>
2018-09-29 08:42:39 -0500

    one LSTM instead of two

--- relation_extraction_pubtator/machine_learning_models/tf_recurrent.py
+++ relation_extraction_pubtator/machine_learning_models/tf_recurrent.py
@@ -113,8 +113,8 @@ def recurrent_train(features, labels, num_dep_types, num_path_words, model_dir, 
     with tf.name_scope("dependency_type_embedding"):
         print(num_dep_types)
         W = tf.Variable(tf.random_uniform([num_dep_types, dep_embedding_dimension]), name="W")
-        word_zeroes = tf.Variable(tf.constant(0.0,shape=[num_path_words,word_embedding_dimension]))
-        embedded_dep = tf.concat([tf.nn.embedding_lookup(word_zeroes,batch_word_ids),tf.nn.embedding_lookup(W, batch_dependency_ids)],axis = 2)
+        word_zeroes = tf.Variable(tf.constant(0.0,shape=[-1,100,word_embedding_dimension]))
+        embedded_dep = tf.concat([word_zeroes,tf.nn.embedding_lookup(W, batch_dependency_ids)],axis = 2)
         print(embedded_dep.shape)
 
         dep_embedding_saver = tf.train.Saver({"dep_embedding/W": W})
@@ -126,8 +126,8 @@ def recurrent_train(features, labels, num_dep_types, num_path_words, model_dir, 
             W = tf.Variable(tf.constant(0.0, shape=[num_path_words, word_embedding_dimension]), name="W")
             embedding_placeholder = tf.placeholder(tf.float32, [num_path_words, word_embedding_dimension])
             embedding_init = W.assign(embedding_placeholder)
-            dep_zeroes = tf.Variable(tf.constant(0.0, shape=[num_dep_types, dep_embedding_dimension]))
-            embedded_word = tf.concat([tf.nn.embedding_lookup(W, batch_word_ids),tf.nn.embedding_lookup(dep_zeroes,batch_dependency_ids)],axis=2)
+            dep_zeroes = tf.Variable(tf.constant(0.0, shape=[-1,100, dep_embedding_dimension]))
+            embedded_word = tf.concat([tf.nn.embedding_lookup(W, batch_word_ids),dep_zeroes],axis=2)
 
             word_embedding_saver = tf.train.Saver({"dependency_word_embedding/W": W})
 
@@ -135,8 +135,8 @@ def recurrent_train(features, labels, num_dep_types, num_path_words, model_dir, 
     else:
         with tf.name_scope("dependency_word_embedding"):
             W = tf.Variable(tf.random_uniform([num_path_words, word_embedding_dimension]), name="W")
-            dep_zeroes = tf.Variable(tf.constant(0.0, shape=[num_dep_types, dep_embedding_dimension]))
-            embedded_word = tf.concat([tf.nn.embedding_lookup(W, batch_word_ids), tf.nn.embedding_lookup(dep_zeroes, batch_dependency_ids)],axis=2)
+            dep_zeroes = tf.Variable(tf.constant(0.0, shape=[-1,100, dep_embedding_dimension]))
+            embedded_word = tf.concat([tf.nn.embedding_lookup(W, batch_word_ids), dep_zeroes],axis=2)
             word_embedding_saver = tf.train.Saver({"dependency_word_embedding/W": W})
 
     with tf.name_scope("word_dropout"):
